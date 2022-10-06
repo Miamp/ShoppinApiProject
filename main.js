88c97da48ae8d6a9359b4cc33a0a7db458af8dad
Mildred <millymartz23@gmai.com>
2022-10-06 18:27:54 -0500

    Finish v1

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ async function allProducts() {
             <div class="card-body">
                 <h5 class="card-title">${elemento.title}</h5>
                 <p class="card-text">${elemento.price}</p>
-                <a href="#" class="btn btn-primary">Detalles</a>
             </div>
         </div>
     `
@@ -31,9 +30,9 @@ async function Electronicos (){
     var respuestaElectronicos = await fetch (allelectronicos);
     var respElectronicos = await respuestaElectronicos.json();
     var electronicos = respElectronicos;
-    console.log(respElectronicos);
 
     let contenido = document.getElementById("contenido")
+    contenido.innerHTML = ""
     electronicos.forEach((elemento, indice, arreglo) => {
         contenido.innerHTML +=
             `
@@ -42,11 +41,99 @@ async function Electronicos (){
             <div class="card-body">
                 <h5 class="card-title">${elemento.title}</h5>
                 <p class="card-text">${elemento.price}</p>
-                <a href="#" class="btn btn-primary">Detalles</a>
             </div>
         </div>
     `
 
     }) 
 
+}
+
+async function Joyeria(){
+    var allJoyeria = "https://fakestoreapi.com/products/category/jewelery";
+    var respuestaJoyeria = await fetch (allJoyeria);
+    var respJoyeria = await respuestaJoyeria.json();
+    var joyeria = respJoyeria;
+    let contenido = document.getElementById("contenido")
+    contenido.innerHTML = ""
+    joyeria.forEach((elemento, indice, arreglo) => {
+        contenido.innerHTML +=
+            `
+        <div class="card cardDetalles" style="width: 13rem;">
+            <img class="imagen" src=${elemento.image} class="card-img-top" alt="${elemento.id}">
+            <div class="card-body">
+                <h5 class="card-title">${elemento.title}</h5>
+                <p class="card-text">${elemento.price}</p>
+            </div>
+        </div>
+    `
+
+    }) 
+}
+
+async function ropaH(){
+    var allRopaH = "https://fakestoreapi.com/products/category/men's clothing";
+    var respuestaRopaH = await fetch (allRopaH);
+    var respRopaH = await respuestaRopaH.json()
+    var ropaH = respRopaH;
+    let contenido = document.getElementById("contenido")
+    contenido.innerHTML = ""
+    ropaH.forEach((elemento, indice, arreglo) => {
+        contenido.innerHTML +=
+            `
+        <div class="card cardDetalles" style="width: 13rem;">
+            <img class="imagen" src=${elemento.image} class="card-img-top" alt="${elemento.id}">
+            <div class="card-body">
+                <h5 class="card-title">${elemento.title}</h5>
+                <p class="card-text">${elemento.price}</p>
+            </div>
+        </div>
+    `
+
+    }) 
+}
+
+async function ropaM(){
+    var allRopaM = "https://fakestoreapi.com/products/category/women's clothing"
+    var respuestaRopaM = await fetch (allRopaM);
+    var resRopaM = await respuestaRopaM.json();
+    var ropaM = resRopaM;
+    let contenido = document.getElementById("contenido")
+    contenido.innerHTML = ""
+    ropaM.forEach((elemento, indice, arreglo) => {
+        contenido.innerHTML +=
+            `
+        <div class="card cardDetalles" style="width: 13rem;">
+            <img class="imagen" src=${elemento.image} class="card-img-top" alt="${elemento.id}">
+            <div class="card-body">
+                <h5 class="card-title">${elemento.title}</h5>
+                <p class="card-text">${elemento.price}</p>
+            </div>
+        </div>
+    `
+
+    })
+}
+
+async function inicio() {
+    var allProducts = "https://fakestoreapi.com/products?limit=8";
+    var respuestaAllProducts = await fetch(allProducts);
+    var respJsonP = await respuestaAllProducts.json();
+    var productos = respJsonP;
+    let contenido = document.getElementById("contenido")
+    contenido.innerHTML = ""
+
+    productos.forEach((elemento, indice, arreglo) => {
+        contenido.innerHTML +=
+            `
+        <div class="card cardDetalles" style="width: 13rem;">
+            <img class="imagen" src=${elemento.image} class="card-img-top" alt="${elemento.id}">
+            <div class="card-body">
+                <h5 class="card-title">${elemento.title}</h5>
+                <p class="card-text">${elemento.price}</p>
+            </div>
+        </div>
+    `
+
+    })
 }
